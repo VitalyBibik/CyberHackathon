307926dcc5b9ffe105485465649f3866fa833d57
Vitaly Bibik <wital2010@gmail.com>
2020-12-26 03:14:31 +0300

    Started player

---
+++ src/components/audio-player/audio-player.js
@@ -0,0 +1,67 @@
+import React, { useRef, useState } from 'react';
+import cn from 'classnames';
+import styles from './audio-player.module.css';
+import ctm from '../../utils/convert-to-minutes';
+import throttling from '../../utils/throttling';
+import PlayerTimeline from '../sound-timeline';
+
+function SoundPlayer({ onClick, togglePlay }) {
+
+  const [isPlaying, setIsPlaying] = useState(false); // play or pause
+  const myPlayer = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0); //  time song
+  const [duration, setDuration] = useState(0);
+
+  const canPlay = useRef(false);
+
+  const onTimeUpdate = throttling((e) => {
+    setCurrentTime(e.target.currentTime);
+  }, 1000);
+
+  const onPlay = (e) => {
+    setDuration(e.target.duration);
+  };
+  const onPause = (e) => {
+    setDuration(e.target.duration);
+    canPlay.current = false;
+  };
+
+  return (
+      <>
+      <div className={cn(styles['player'], styles['player-is_open'])}>
+        <span
+          onClick={(_) => {
+            if (isPlaying) {
+              myPlayer.current.pause();
+              setIsPlaying(false);
+              togglePlay(isPlaying);
+            } else {
+              myPlayer.current.play();
+              canPlay.current = true;
+              setIsPlaying(true);
+              togglePlay(isPlaying);
+            }
+          }}
+          className={cn(styles['icon'], styles['icon_block_controls'])}
+        >
+          {isPlaying ? (
+            'pause'
+          ) : (
+           'play'
+          )}
+        </span>
+
+          </div>
+          <PlayerTimeline
+            currentTime={currentTime}
+            duration={duration}
+            onClick={(time) => {
+              myPlayer.current.currentTime = time;
+            }}
+          />
+
+      </>
+  );
+}
+
+export default SoundPlayer;
--- src/components/pages/section-voice/section-voice.js
+++ src/components/pages/section-voice/section-voice.js
@@ -3,11 +3,13 @@ import React from 'react';
 import SectionTitle from '../../section-title';
 import Button from "../../../ui/button";
 import SvgMicro from "../../svg-icons/svg-micro";
+import AudioPlayer from '../../audio-player/audio-player'
 
 function SectionVoice({sectionVoice, buttonText}) {
   return (
       <>
         <SectionTitle title={sectionVoice.titleText[0].title}/>
+         <AudioPlayer />
         <Button button={buttonText.mainButton[1]} icon={SvgMicro}/>
       </>
   );
