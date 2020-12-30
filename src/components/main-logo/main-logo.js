60df9f8d7c90dd2f30718717fa0fa983587f90a7
Vitaly Bibik <wital2010@gmail.com>
2020-12-30 13:08:20 +0300

    Remove divs

--- src/components/main-logo/main-logo.js
+++ src/components/main-logo/main-logo.js
@@ -1,18 +1,16 @@
 import styles from './main-logo.module.scss';
 import React from 'react';
 import cn from 'classnames';
-import SvgNumber from '../svg-icons/svg-Number'
+import SvgNumber from '../svg-icons/svg-Number';
 
-const MainLogo = ({mainLogo}) => {
-       return (
-           <div>
-           <h1 className={cn(styles['main-logo'])}>{mainLogo[0].title}
-               <span className={cn(styles['number-icon'])}>
-                   <SvgNumber/>
-               </span>
-           </h1>
-           </div>
-       )
-
-}
+const MainLogo = ({ mainLogo }) => {
+  return (
+    <h1 className={cn(styles['main-logo'])}>
+      {mainLogo[0].title}
+      <span className={cn(styles['number-icon'])}>
+        <SvgNumber />
+      </span>
+    </h1>
+  );
+};
 export default MainLogo;
--- src/components/section-title/section-title.js
+++ src/components/section-title/section-title.js
@@ -3,7 +3,6 @@ import React from 'react';
 import cn from 'classnames';
 
 const SectionTitle = ({ title }) => {
-  console.log('Sect-Title', title);
   return <h2 className={cn(styles['section-title'])}>{title}</h2>;
 };
 
